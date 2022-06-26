564c0708a8bd1532b8fa39898c7a2f80155f6ad3
Pradeep-SG <pradeepsg612@gmail.com>
2022-06-26 22:18:24 +0530

    Prepare for deployment

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
     <nav className={`${classes.nav} ${toggle ? classes['show-navbar'] : ''}`}>
       <div className={classes['nav-brand']}>
         <h1>
-          <Link to="/">ShopXo</Link>
+          <Link to="/">Levanto</Link>
         </h1>
         <ClickAwayListener
           onClickAway={() => {
